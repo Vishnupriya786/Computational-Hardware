faefac5b5928a2dae0ac6bf32bb828c3502f2abc
Vishnupriya786 <68602337+Vishnupriya786@users.noreply.github.com>
2021-04-18 21:01:30 +0530

    Update control any digital output using Voice commands.ino
    4. Use Arduino and Bluetooth device to control any digital output using Voice commands

--- control any digital output using Voice commands.ino
+++ control any digital output using Voice commands.ino
@@ -1,1 +1,28 @@
-
+String voice;
+int
+led1 = 13; //Connect LED 1 To Pin #2
+//--------------------------Call A Function-------------------------------//
+void allon(){
+     digitalWrite(led1, HIGH);
+}
+void alloff(){
+     digitalWrite(led1, LOW);
+}
+//-----------------------------------------------------------------------//
+void setup() {
+  Serial.begin(9600);
+  pinMode(led1, OUTPUT);
+}
+//-----------------------------------------------------------------------//
+void loop() {
+  while (Serial.available()){  //Check if there is an available byte to read
+  delay(10); //Delay added to make thing stable
+  char c = Serial.read(); //Conduct a serial read
+  if (c == '#') {break;} //Exit the loop when the # is detected after the word
+  voice += c; //Shorthand for voice = voice + c
+  }
+  if (voice.length() > 0) {
+    Serial.println(voice);
+  if(voice == "*start") {digitalWrite(led1, HIGH);}
+  else if(voice == "*stop") {digitalWrite(led1, LOW);}
+voice="";}} 
